00159078fd2fe3ee6229dfc8ec6ec4de4410ea43
Yogesh <yogeshbietjhs@gmail.com>
2022-07-20 22:10:35 +0530

    Edit and remove

--- CreateContact.cs
+++ CreateContact.cs
@@ -19,10 +19,10 @@ namespace AddressBookSystem
                 Console.ReadKey();
                 return;
             }
-            Console.WriteLine("Here are the current people in your address book:\n");
-            foreach (var person in People)
+            Console.WriteLine("Here are the current people in your address book:");
+            for (int i = 0; i < People.Count; i++)
             {
-                PrintPerson(person);
+                PrintPerson(People[i]);
             }
             Console.WriteLine("\nPress any key to continue.");
             Console.ReadKey();
@@ -30,33 +30,42 @@ namespace AddressBookSystem
 
 
 
-        // UC-2 This method will Add the Person Contact.
-        
-        public void AddPerson()
+        //UC-2 This method will Add the Person Contact.
+        public void AddPerson(int a = -1)
         {
+
             Person person = new Person();
 
             Console.Write("Enter First Name: ");
-            person.FirstName = Console.ReadLine();
+            person.FirstName = Console.ReadLine().Trim();
 
             Console.Write("Enter Last Name: ");
-            person.LastName = Console.ReadLine();
+            person.LastName = Console.ReadLine().Trim();
 
             Console.Write("Enter Phone Number: ");
-            person.PhoneNumber = Console.ReadLine();
+            person.PhoneNumber = Console.ReadLine().Trim();
 
-            Console.Write("Write Your Address, Enter Area/Colony Name: ");
-            string[] addresses = new string[4];
-            addresses[0] = Console.ReadLine();
+            Console.Write("Enter Your Address\nEnter Area/Colony Name: ");
+
+            person.Area = Console.ReadLine().Trim();
             Console.Write("Enter CityName: ");
-            addresses[1] = Console.ReadLine();
+            person.City = Console.ReadLine().Trim();
             Console.Write("Enter State: ");
-            addresses[2] = Console.ReadLine();
+            person.State = Console.ReadLine().Trim();
             Console.Write("Enter ZipCode: ");
-            addresses[3] = Console.ReadLine();
-            person.Addresses = addresses;
+            person.ZipCode = Console.ReadLine().Trim();
 
-            People.Add(person);
+
+            if (a == -1)
+            {
+                People.Add(person);
+            }
+            else
+            {
+                People.Insert(a, person);
+            }
+
+
         }
 
 
@@ -66,11 +75,72 @@ namespace AddressBookSystem
             Console.WriteLine("First Name: " + person.FirstName);
             Console.WriteLine("Last Name: " + person.LastName);
             Console.WriteLine("Phone Number: " + person.PhoneNumber);
-            Console.WriteLine("Address: Area " + person.Addresses[0]);
-            Console.WriteLine(" City: " + person.Addresses[1]);
-            Console.WriteLine(" State: " + person.Addresses[2]);
-            Console.WriteLine(" ZipCOde: " + person.Addresses[3]);
-            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Address: Area " + person.Area);
+            Console.WriteLine("City: " + person.City);
+            Console.WriteLine("State: " + person.State);
+            Console.WriteLine("ZipCOde: " + person.ZipCode);
+        }
+
+        //uc-3
+        public void Edit(string Name)
+        {
+
+            for (int i = 0; i < People.Count; i++)
+            {
+                if (People[i].FirstName +" "+ People[i].LastName == Name)
+                {
+                    Console.WriteLine("Which thing do you want to change: ");
+                    Console.WriteLine("Enter \'1\' for First Name\n\'2\' for LastName\n\'3 for Phone Number\n\'4\' for Address\n\'5\' for All");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    switch (num)
+                    {
+                        case 1:
+                            Console.Write("Enter the First Name: ");
+                            People[i].FirstName = Console.ReadLine();
+                            Console.WriteLine();
+                            PrintPerson(People[i]);
+                            break;
+                        case 2:
+                            Console.Write("Enter the Last Name: ");
+                            People[i].LastName = Console.ReadLine();
+                            Console.WriteLine();
+                            PrintPerson(People[i]);
+                            break;
+                        case 3:
+                            Console.Write("Enter Phone Number: ");
+                            People[i].PhoneNumber = Console.ReadLine();
+                            Console.WriteLine();
+                            PrintPerson(People[i]);
+                            break;
+                        case 4:
+                            Console.Write("Enter Area/Colony Name: ");
+                            People[i].Area = Console.ReadLine();
+                            Console.WriteLine("Enter City Name: ");
+                            People[i].City = Console.ReadLine();
+                            Console.WriteLine("Enter State Name: ");
+                            People[i].State = Console.ReadLine();
+                            Console.WriteLine("Enter Zip Code: ");
+                            People[i].ZipCode = Console.ReadLine();
+                            Console.WriteLine();
+                            PrintPerson(People[i]);
+                            break;
+                        case 5:
+                            AddPerson(i);
+                            Console.WriteLine();
+                            PrintPerson(People[i]);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Number:");
+                            break;
+                    }
+
+                }
+
+            }
+
+
+
+
         }
     }
 
@@ -79,6 +149,12 @@ namespace AddressBookSystem
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-        public string[] Addresses { get; set; }
+        public string Area { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
     }
+
+
+
 }
